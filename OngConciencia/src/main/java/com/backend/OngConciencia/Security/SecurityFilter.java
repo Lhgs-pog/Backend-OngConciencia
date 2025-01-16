354e576599c702127c5d775e803c9169ad6df5cf
Daniel Umberto <danielumberto51@gmail.com>
2025-01-16 16:55:52 -0300

    fix: Sintaxe errada

--- OngConciencia/src/main/java/com/backend/OngConciencia/Security/SecurityFilter.java
+++ OngConciencia/src/main/java/com/backend/OngConciencia/Security/SecurityFilter.java
@@ -10,16 +10,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
+@Component
 public class SecurityFilter extends OncePerRequestFilter {
 
     @Autowired
     private TokenService tokenService;
 
     @Autowired
-    UsuarioRepository usuarioRepository;
+    private UsuarioRepository usuarioRepository;
 
 
     @Override
@@ -36,7 +38,7 @@ public class SecurityFilter extends OncePerRequestFilter {
     }
 
     private String recoveryToken(HttpServletRequest request) {
-        String authHeader = request.getHeader("Authorization");
+        var authHeader = request.getHeader("Authorization");
         if (authHeader == null) return null;
         return authHeader.replace("Bearer ", "");
     }
